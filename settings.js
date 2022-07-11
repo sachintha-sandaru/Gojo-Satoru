a1a058dd090d961af44e1a6936d49e44cca9e5c1
sachintha-sandaru <109032572+sachintha-sandaru@users.noreply.github.com>
2022-07-11 13:27:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,14 +19,14 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['+94767361910']
+global.premium = ['+94767361910']
+global.ownernomer = '+94767361910'
 global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.botname = 'Real Sachiya'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'Sri Lanka'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
